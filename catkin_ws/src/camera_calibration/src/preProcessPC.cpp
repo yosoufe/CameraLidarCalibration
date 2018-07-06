f33937cf3a98008cb87e89a7034d035887475e5d
Yousof <yousofebneddin@yahoo.com>
2018-07-06 10:01:22 -0700

    crop the surface from the point cloud"

--- catkin_ws/src/camera_calibration/src/preProcessPC.cpp
+++ catkin_ws/src/camera_calibration/src/preProcessPC.cpp
@@ -26,55 +26,56 @@ Eigen::Vector4f p1,p2;
 
 void pc2Callback(const sensor_msgs::PointCloud2ConstPtr& input)
 {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PCLPointCloud2* cloud2 = new pcl::PCLPointCloud2;
-  pcl::PCLPointCloud2ConstPtr cloudPtr(cloud2);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr PCLXYZcloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PCLPointCloud2* PCLcloud2 = new pcl::PCLPointCloud2;
+  pcl::PCLPointCloud2ConstPtr cloudPtr(PCLcloud2);
   pcl::PCLPointCloud2 cloud_filtered;
-  pcl_conversions::toPCL(*input, *cloud2);
-  pcl::fromPCLPointCloud2(*cloud2,*cloud);
+  pcl_conversions::toPCL(*input, *PCLcloud2);
+  pcl::fromPCLPointCloud2(*PCLcloud2,*PCLXYZcloud);
+
+  // crop ROI
+  pcl::CropBox<pcl::PointXYZ> cropFilter;
+  cropFilter.setInputCloud(PCLXYZcloud);
+  cropFilter.setMin(p1);
+  cropFilter.setMax(p2);
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pclCropped(new pcl::PointCloud<pcl::PointXYZ>);
+  cropFilter.filter(*pclCropped);
+
+  // Create a container for the data.
+  sensor_msgs::PointCloud2Ptr ROSoutput(new sensor_msgs::PointCloud2);
+  //pcl::toROSMsg(*pclCropped,*ROSoutput);
+
+  // Planar Segmentation
   pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
   pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
   pcl::SACSegmentation<pcl::PointXYZ> seg;
-
   seg.setOptimizeCoefficients (true);
-
   seg.setModelType (pcl::SACMODEL_PLANE);
   seg.setMethodType (pcl::SAC_RANSAC);
   seg.setDistanceThreshold (0.01);
-
-  seg.setInputCloud (cloud);
+  seg.setInputCloud (pclCropped);
   seg.segment (*inliers, *coefficients);
-
   if (inliers->indices.size () == 0)
   {
     ROS_DEBUG ("Could not estimate a planar model for the given dataset.");
     return;
   }
-
   ROS_INFO_STREAM(inliers->indices.size ()<<"Model coefficients: " << coefficients->values[0] << " "
                                   << coefficients->values[1] << " "
                                   << coefficients->values[2] << " "
                                   << coefficients->values[3]);
+  // Just Inliers
+  pcl::CropBox<pcl::PointXYZ> indFilter;
+  cropFilter.setInputCloud(pclCropped);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pclPointsOnSurface(new pcl::PointCloud<pcl::PointXYZ>);
+  cropFilter.setIndices(inliers);
+  cropFilter.filter(*pclPointsOnSurface);
 
+  pcl::toROSMsg(*pclPointsOnSurface,*ROSoutput);
 
-  pcl::CropBox<pcl::PointXYZ> cropFilter;
-  cropFilter.setInputCloud(cloud);
-  cropFilter.setMin(p1);
-  cropFilter.setMax(p2);
-
-  pcl::PointCloud<pcl::PointXYZ>::Ptr pclOtput(new pcl::PointCloud<pcl::PointXYZ>);
-  cropFilter.filter(*pclOtput);
-
-  // Create a container for the data.
-  sensor_msgs::PointCloud2Ptr output(new sensor_msgs::PointCloud2);
-
-  pcl::toROSMsg(*pclOtput,*output);
-
-  // Do data processing here...
-  //output = *input;
-
-  // Publish the data.
-  pub.publish (output);
+  // Publish the Output.
+  pub.publish (ROSoutput);
 }
 
 int main(int argc, char **argv)
--- readme.md
+++ readme.md
@@ -7,8 +7,9 @@ The Assigment is the 2 first tasks of [this](https://docs.google.com/document/d/
 * `rqt_bag` GUI has some issues on ROS melodic and Ubuntu bionic (check [here](https://github.com/ros-visualization/rqt_bag/issues/27)). Only `rosbag` in terminal could be used.
 * Sample some of the [images](https://github.com/yosoufe/Assignment/tree/master/scripts/cal_imgs) for calibration using rqt_image_viewer.
 * Camera Calibration in this [Notebook](https://github.com/yosoufe/Assignment/blob/master/scripts/Camera%20Calibration.ipynb). ([hint](https://docs.opencv.org/3.4/dc/dbb/tutorial_py_calibration.html))
-
-* Pose Estimation ([hint](https://docs.opencv.org/3.4/d7/d53/tutorial_py_pose.html))
+* Croping the Pointcloud to the ROI which has the checkerboard in [this ROS Node](https://github.com/yosoufe/Assignment/blob/master/catkin_ws/src/camera_calibration/src/preProcessPC.cpp).
+* SubSample the cropped pintcloud to only the checkerboard surface in [this ROS Node](https://github.com/yosoufe/Assignment/blob/master/catkin_ws/src/camera_calibration/src/preProcessPC.cpp).
+* Pose Estimation with camera and checkerboard ([hint](https://docs.opencv.org/3.4/d7/d53/tutorial_py_pose.html))
 
 ## Installations:
 
@@ -24,7 +25,8 @@ conda install -c conda-forge opencv
 Put the bag file in a folder `data` in the same directory. 
 Then it can be played by the following command.
 ```
-rosbag play -q data/2016-11-22-14-32-13_test.bag
+source devel/setup.bash
+roslaunch camera_calibration playBag.launch
 ```
 
 
