ec918aedd9d59e8cbe1831f0d9cfa1e56969cb81
Yousof <yousofebneddin@yahoo.com>
2018-07-05 22:20:00 -0700

    cleaning up the git

--- catkin_ws/build/camera_calibration/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yousof/RideCellAssignment/catkin_ws/src/camera_calibration
-# Build directory: /home/yousof/RideCellAssignment/catkin_ws/build/camera_calibration
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
